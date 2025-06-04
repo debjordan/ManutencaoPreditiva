1b92bcaa09a207ef51124101f024478f58818c76
Jordan <joaojordan.iam@gmail.com>
2025-06-04 20:25:20 -0300

    feat: incluir service no container de injecao de dependencia.

--- backend/ManutencaoPreditiva.Api/API/Configuration/DependencyInjection.cs
+++ backend/ManutencaoPreditiva.Api/API/Configuration/DependencyInjection.cs
@@ -1,9 +1,13 @@
 using ManutencaoPreditiva.Api.Application.Services;
 using ManutencaoPreditiva.Api.Infrastructure.Data;
 using ManutencaoPreditiva.Api.Infrastructure.Mqtt;
+using ManutencaoPreditiva.Api.Infrastructure.Repositories.Abstractions;
+using ManutencaoPreditiva.Api.Infrastructure.Repositories.Implementations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+
 namespace ManutencaoPreditiva.Api.Configuration;
+
 public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, string connectionString)
@@ -11,12 +15,20 @@ public static class DependencyInjection
         if (string.IsNullOrEmpty(connectionString))
             throw new ArgumentNullException(nameof(connectionString));
 
+        // Configurar DateTime para UTC
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
         services.AddDbContext<AppDbContext>(options =>
             options.UseNpgsql(connectionString));
-        services.AddSingleton<MqttService>();
-        services.AddScoped<SensorService>();
+
+        services.AddScoped<ISensorRepository, SensorRepository>();
+
+        services.AddScoped<ISensorService, SensorService>();
         services.AddScoped<ProductionService>();
         services.AddScoped<PredictionService>();
+
+        services.AddSingleton<MqttService>();
+
         return services;
     }
 }
