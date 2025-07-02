33b5ae00dacaa5cfa60c8f7ed30471e4b16ad79f
Jordan <joaojordan.iam@gmail.com>
2025-07-01 22:07:59 -0300

    feat: criar modulo de cadastros de setores.

--- src/API/Controllers/SectorController.cs
+++ src/API/Controllers/SectorController.cs
@@ -1,87 +1,87 @@
-using ManutencaoPreditiva.Application.Common;
-using ManutencaoPreditiva.Application.DTOs.Request;
-using ManutencaoPreditiva.Application.Interfaces.Services;
-using Microsoft.AspNetCore.Mvc;
+// using ManutencaoPreditiva.Application.Common;
+// using ManutencaoPreditiva.Application.DTOs.Request;
+// using ManutencaoPreditiva.Application.Interfaces.Services;
+// using Microsoft.AspNetCore.Mvc;
 
-namespace ManutencaoPreditiva.Api.Controllers;
+// namespace ManutencaoPreditiva.Api.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class SectorController : ControllerBase
-{
-    private readonly ISectorApplicationService _sectorApplicationService;
+// [ApiController]
+// [Route("api/[controller]")]
+// public class SectorController : ControllerBase
+// {
+//     private readonly ISectorApplicationService _sectorApplicationService;
 
-    public SectorController(ISectorApplicationService sectorApplicationService)
-    {
-        _sectorApplicationService = sectorApplicationService;
-    }
+//     public SectorController(ISectorApplicationService sectorApplicationService)
+//     {
+//         _sectorApplicationService = sectorApplicationService;
+//     }
 
-    [HttpGet]
-    public async Task<IActionResult> GetAllSectorsAsync()
-    {
-        var result = await _sectorApplicationService.GetAllSectorsAsync();
+//     [HttpGet]
+//     public async Task<IActionResult> GetAllSectorsAsync()
+//     {
+//         var result = await _sectorApplicationService.GetAllSectorsAsync();
 
-        if (!result.IsSuccess)
-            return ResponseHandler.HandlerResult(result);
+//         if (!result.IsSuccess)
+//             return ResponseHandler.HandlerResult(result);
 
-        return Ok(result.Value);
-    }
+//         return Ok(result.Value);
+//     }
 
-    [HttpGet("{id:guid}")]
-    public async Task<IActionResult> GetSectorByIdAsync(Guid id)
-    {
-        var result = await _sectorApplicationService.GetSectorByIdAsync(id);
+//     [HttpGet("{id:guid}")]
+//     public async Task<IActionResult> GetSectorByIdAsync(Guid id)
+//     {
+//         var result = await _sectorApplicationService.GetSectorByIdAsync(id);
 
-        if (!result.IsSuccess)
-            return ResponseHandler.HandlerResult(result);
+//         if (!result.IsSuccess)
+//             return ResponseHandler.HandlerResult(result);
 
-        return Ok(result.Value);
-    }
+//         return Ok(result.Value);
+//     }
 
-    [HttpGet("active")]
-    public async Task<IActionResult> GetActiveSectosAsync()
-    {
-        var result = await _sectorApplicationService.GetActiveSectorsAsync();
+//     [HttpGet("active")]
+//     public async Task<IActionResult> GetActiveSectosAsync()
+//     {
+//         var result = await _sectorApplicationService.GetActiveSectorsAsync();
 
-        if (!result.IsSuccess)
-            return ResponseHandler.HandlerResult(result);
+//         if (!result.IsSuccess)
+//             return ResponseHandler.HandlerResult(result);
 
-        return Ok(result.Value);
-    }
+//         return Ok(result.Value);
+//     }
 
-    [HttpPost]
-    public async Task<IActionResult> CreateSectorAsync([FromBody] CreateSectorDto dto)
-    {
-        var result = await _sectorApplicationService.CreateSectorAsync(dto);
+//     [HttpPost]
+//     public async Task<IActionResult> CreateSectorAsync([FromBody] CreateSectorDto dto)
+//     {
+//         var result = await _sectorApplicationService.CreateSectorAsync(dto);
 
-        if (!result.IsSuccess)
-            return ResponseHandler.HandlerResult(result);
+//         if (!result.IsSuccess)
+//             return ResponseHandler.HandlerResult(result);
 
-        return CreatedAtAction(
-            nameof(GetSectorByIdAsync),
-            new { id = result.Value!.Id },
-            result.Value);
-    }
+//         return CreatedAtAction(
+//             nameof(GetSectorByIdAsync),
+//             new { id = result.Value!.Id },
+//             result.Value);
+//     }
 
-    [HttpPut("{id:guid}")]
-    public async Task<IActionResult> UpdateSectorByIdAsync(Guid id, [FromBody] UpdateSectorDto dto)
-    {
-        var result = await _sectorApplicationService.UpdateSectorByIdAsync(id, dto);
+//     [HttpPut("{id:guid}")]
+//     public async Task<IActionResult> UpdateSectorByIdAsync(Guid id, [FromBody] UpdateSectorDto dto)
+//     {
+//         var result = await _sectorApplicationService.UpdateSectorByIdAsync(id, dto);
 
-        if (!result.IsSuccess)
-            return ResponseHandler.HandlerResult(result);
+//         if (!result.IsSuccess)
+//             return ResponseHandler.HandlerResult(result);
 
-        return Ok(result.Value);
-    }
+//         return Ok(result.Value);
+//     }
 
-    [HttpDelete("{id:guid}")]
-    public async Task<IActionResult> DeleteSectorByIdAsync(Guid id)
-    {
-        var result = await _sectorApplicationService.DeleteSectorByIdAsync(id);
+//     [HttpDelete("{id:guid}")]
+//     public async Task<IActionResult> DeleteSectorByIdAsync(Guid id)
+//     {
+//         var result = await _sectorApplicationService.DeleteSectorByIdAsync(id);
 
-        if (!result.IsSuccess)
-            return ResponseHandler.HandlerResult(result);
+//         if (!result.IsSuccess)
+//             return ResponseHandler.HandlerResult(result);
 
-        return NoContent();
-    }
-}
+//         return NoContent();
+//     }
+// }
