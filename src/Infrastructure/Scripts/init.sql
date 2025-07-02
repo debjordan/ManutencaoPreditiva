7d513ef2894647601d3cc7a335894a5fdd9b198a
Jordan <joaojordan.iam@gmail.com>
2025-07-01 22:08:43 -0300

    refactor: atualizar script de criação de tabelas postgresql.

--- src/Infrastructure/Scripts/init.sql
+++ src/Infrastructure/Scripts/init.sql
@@ -1,26 +1,5 @@
--- CREATE TABLE sensors (
---     id SERIAL PRIMARY KEY,
---     machine_id VARCHAR(50),
---     vibration FLOAT,
---     temperature FLOAT,
---     timestamp TIMESTAMP
--- );
-
--- CREATE TABLE production (
---     id SERIAL PRIMARY KEY,
---     machine_id VARCHAR(50),
---     cycle_time FLOAT,
---     man_time FLOAT,
---     machine_time FLOAT,
---     availability FLOAT,
---     performance FLOAT,
---     quality FLOAT,
---     timestamp TIMESTAMP
--- );
-
--- CREATE TABLE predictions (
---     id SERIAL PRIMARY KEY,
---     machine_id VARCHAR(50),
---     failure_probability FLOAT,
---     timestamp TIMESTAMP
--- );
+CREATE DATABASE IF NOT EXISTS "Registrys";
+-- CREATE DATABASE IF NOT EXISTS "ManutencaoPreditiva";
+-- CREATE DATABASE IF NOT EXISTS "OrderServiceDb";
+-- CREATE DATABASE IF NOT EXISTS "UserServiceDb";
+-- CREATE DATABASE IF NOT EXISTS "LogsDb";
